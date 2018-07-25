3962f94708f04dc2036eb8e92bb2840ec060cf55
Bertrand Marechal <bertrand.marechal@gadz.org>
2018-07-25 08:47:15 +0100

    fix the event discovery for middle tier

--- docker/postgres/scripts/02-functions-017-mgtf_update_middle_tier_files.sql
+++ docker/postgres/scripts/02-functions-017-mgtf_update_middle_tier_files.sql
@@ -17,33 +17,51 @@ BEGIN
                     AND mtf_service_name = (i_serverless_file->>'serviceName')::TEXT
             WHERE rep_folder_name = i_repo_name
     ), delete_middle_tier_file as (
+            -- delete the existing file
             delete from mgtt_middle_tier_file_mtf
             where pk_mtf_id = (
                     select pk_mtf_id
                     from file_data
             ) returning pk_mtf_id
     ), insert_data_file AS (
+            -- insert the new file
             INSERT INTO mgtt_middle_tier_file_mtf (fk_rep_mtf_repository_id,mtf_file_name,mtf_service_name)
             SELECT pk_rep_id, i_parent_folder, service_name
             FROM file_data
             LEFT JOIN delete_middle_tier_file ON TRUE
             returning pk_mtf_id
+    ), insert_functions AS (
+            -- insert the functions
+        INSERT INTO mgtt_middle_tier_function_mlf (
+                fk_mtf_mlf_middle_tier_file_id,
+                mlf_name,
+                mlf_file_name,
+                mlf_handler_name
+        )
+        SELECT pk_mtf_id, (func::json)->>'name', (func::json)->>'handler', (func::json)->>'handlerFunctionName'
+        FROM insert_data_file
+        INNER JOIN (
+                SELECT json_array_elements(functions) as func
+                FROM file_data
+        ) a ON TRUE
+        RETURNING pk_mlf_id, mlf_name
     ), function_data AS (
+            -- get function data ready for event insert
             SELECT pk_mlf_id, mlf_name
             FROM (
                     SELECT json_array_elements(functions) as func
                     FROM file_data
             ) a
             INNER JOIN file_data on TRUE
-            INNER JOIN mgtt_middle_tier_function_mlf
-                    ON fk_mtf_mlf_middle_tier_file_id = pk_mtf_id
-                    AND mlf_name = (func::json)->>'name'
+            INNER JOIN insert_functions
+                    ON mlf_name = (func::json)->>'name'
             LEFT JOIN insert_data_file ON TRUE
     ), insert_events AS (
+            -- insert events
             INSERT INTO mgtt_lambda_function_event_lfe (fk_mlf_lfe_middle_tier_function_id, lfe_event_type, lfe_parameters)
             SELECT pk_mlf_id, (eve::json)->>'type', ((eve::json)->>'params')::json
             FROM (
-                    select (func::json)->>'name' "name", json_array_elements(((func::json)->>'events')::json) eve--json_array_elements((func::json)->>'events'::json)
+                    select (func::json)->>'name' "name", json_array_elements(((func::json)->>'events')::json) eve
                     from (
                             SELECT json_array_elements(functions) as func
                             FROM file_data
